f586b1b4be7dd182261e32d65a6950ec2796d29c
mthoolen <mjjthoolen@gmail.com>
2019-10-04 18:08:44 +0200

    my solution to the gilded rose refactor kata.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,58 +1,80 @@
 package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
+    private val minimumQuality = 0
+    private val maximumQuality = 50
+    private val betterByAge = listOf("Aged Brie")
+    private val legendaries = listOf("Sulfuras, Hand of Ragnaros")
+    init {
+        require(items.all { it.quality >= minimumQuality }) { "quality must be 0 or over!" }
+    }
 
-    fun updateQuality() {
-        for (i in items.indices) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+    private val itemKindToUpdateFunction = mapOf(
+            isLegendary() to updateLegendary(),
+            isConjured() to updateConjured(),
+            isBackstagePass() to updateBackstagePass(),
+            isBetterWithAge() to updateBetterWithAge(),
+            isDefault() to updateDefault())
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+    fun updateQuality() = items.map { decreaseSellIn(it) }.forEach { getUpdateFunction(it).invoke(it) }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
+    private fun getUpdateFunction(item: Item) =
+            itemKindToUpdateFunction.filter { it.key.invoke(item) }.values.first()
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+    private fun updateDefault() = { item: Item ->
+        decreaseQuality(item)
+        if (sellByHasPassed(item)) decreaseQuality(item)
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+    private fun updateBetterWithAge() = { item: Item ->
+        increaseQuality(item)
+        if (sellByHasPassed(item)) increaseQuality(item)
+    }
+
+    private fun updateBackstagePass() = { item: Item ->
+        when {
+            item.sellIn < 1 -> item.quality = minimumQuality
+            item.sellIn < 6 -> increaseQuality(item, 3)
+            item.sellIn < 11 -> increaseQuality(item, 2)
+            item.sellIn < 50 -> increaseQuality(item)
         }
     }
+
+    private fun updateConjured() = { item: Item ->
+        decreaseQuality(item, 2)
+        if (sellByHasPassed(item)) decreaseQuality(item, 2)
+    }
+
+    private fun updateLegendary() = { _: Item -> Unit }
+
+    private fun isLegendary() = { item: Item -> legendaries.contains(item.name) }
+
+    private fun isConjured() = { item: Item -> item.name.startsWith("Conjured") }
+
+    private fun isBackstagePass() = { item: Item -> item.name == "Backstage passes to a TAFKAL80ETC concert" }
+
+    private fun isBetterWithAge() = { item: Item -> betterByAge.contains(item.name) }
+
+    private fun isDefault() = { item: Item -> !(isLegendary()(item) || isBetterWithAge()(item) || isBackstagePass()(item)) }
+
+    private fun decreaseSellIn(item: Item): Item {
+        item.sellIn -= 1
+        return item
+    }
+
+    private fun decreaseQuality(item: Item) {
+        if (item.quality > minimumQuality) item.quality -= 1
+    }
+
+    private fun increaseQuality(item: Item) {
+        if (item.quality < maximumQuality) item.quality += 1
+    }
+
+    private fun increaseQuality(item: Item, amount: Int) = repeat(amount) { increaseQuality(item) }
+
+    private fun decreaseQuality(item: Item, amount: Int) = repeat(amount) { decreaseQuality(item) }
+
+    private fun sellByHasPassed(item: Item): Boolean = item.sellIn < minimumQuality
 
 }
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,105 @@
 package com.gildedrose
 
-import org.junit.Assert.*
-import org.junit.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+    private fun createFoo() = Item("foo", 0, 0)
 
+    @Test
+    internal fun `added item should be found in store`() {
+        val foo = createFoo()
+        createRoseAndUpdate(foo)
+        assertThat(foo.name).isEqualTo("foo")
     }
 
+    @Test
+    internal fun `quality can't be negative`() {
+        val foo = createFoo()
+        createRoseAndUpdate(foo)
+        assertThat(foo.quality).isEqualTo(0)
+    }
 
+    @Test
+    internal fun `quality can't be more than 50`() {
+        val brie = Item("Aged Brie", 0,50)
+        createRoseAndUpdate(brie)
+        assertThat(brie.quality).isEqualTo(50)
+    }
+
+    @Test
+    internal fun `quality degrades with each passing day`() {
+        val bar = Item("bar", 2, 1)
+        createRoseAndUpdate(bar)
+        assertThat(bar.quality).isEqualTo(0)
+    }
+
+    @Test
+    internal fun `aged brie increases in quality the older it gets`() {
+        val brie = Item("Aged Brie", 2, 0)
+        createRoseAndUpdate(brie)
+        assertThat(brie.quality).isEqualTo(1)
+    }
+
+    @Test
+    internal fun `after sell by date has passed, quality should degrade twice as fast`() {
+        val eww = Item("eww", 1, 10)
+        val rose = createRoseAndUpdate(eww)
+        assertThat(eww.quality).isEqualTo(9)
+
+        rose.updateQuality()
+        assertThat(eww.quality).isEqualTo(7)
+    }
+
+    @Test
+    internal fun `legendary items dont decrease in quality`() {
+        val sulfuras = Item("Sulfuras, Hand of Ragnaros", 0, 50)
+        createRoseAndUpdate(sulfuras)
+        assertThat(sulfuras.quality).isEqualTo(50)
+    }
+
+    @Test
+    internal fun `backstage pass quality increases twice as fast with sell in date within 10 days`() {
+        val pass = Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)
+        createRoseAndUpdate(pass)
+        assertThat(pass.quality).isEqualTo(42)
+    }
+
+    @Test
+    internal fun `backstage pass quality increases three times as fast with sell in date within 5 days`() {
+        val pass = Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)
+        createRoseAndUpdate(pass)
+        assertThat(pass.quality).isEqualTo(43)
+    }
+
+    @Test
+    internal fun `backstage pass quality quality is void after sell by date passes`() {
+        val pass = Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)
+        createRoseAndUpdate(pass)
+        assertThat(pass.quality).isEqualTo(0)
+    }
+
+    @Test
+    internal fun `conjured items degrade twice as fast`() {
+        val conjured = Item("Conjured sword", 10, 40)
+        createRoseAndUpdate(conjured)
+        assertThat(conjured.quality).isEqualTo(38)
+    }
+
+    @Test
+    internal fun `conjured items degrade four times as fast after expiring`() {
+        val conjured = Item("Conjured sword", 0, 40)
+        createRoseAndUpdate(conjured)
+        assertThat(conjured.quality).isEqualTo(36)
+    }
+
+    private fun createRoseAndUpdate(item: Item): GildedRose {
+        val items = arrayOf(item)
+        val app = GildedRose(items)
+        app.updateQuality()
+        return app
+    }
 }
 
 
